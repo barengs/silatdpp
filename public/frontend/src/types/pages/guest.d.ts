18dd16cba9cad874281f1794ed0a5be7eca7456c
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-12-01 20:51:14 +0700

    ADD: Added props type for guest book page

---
+++ public/frontend/src/types/pages/guest.d.ts
@@ -0,0 +1,8 @@
+export interface GuestBookProps {
+    nama_tamu: string,
+    alamat: string,
+    no_telpon: number,
+    institusi_tamu_id: number,
+    divisi_id: number,
+    keperluan: string
+}
