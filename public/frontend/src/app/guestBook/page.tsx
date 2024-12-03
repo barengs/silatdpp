14854f996f40b43512e52625cdf4ee062a483c3a
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-12-03 14:40:30 +0700

    UPDATE: Added SSR

--- public/frontend/src/app/guestBook/page.tsx
+++ public/frontend/src/app/guestBook/page.tsx
@@ -1,19 +1,45 @@
-import GuestBook from '@/components/GuestBook/page'
 import DefaultLayout from '@/components/Layouts/DefaultLayout'
+import GuestBook from '@/pages/GuestBook/page'
+import { GuestBookGetInstance } from '@/types/pages/guest'
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
 import React from 'react'
 
 
-interface GuestBookProps {
+export const getGuestData: GetServerSideProps<{data: GuestBookGetInstance|null}> = async () => {
+
+  try {
+    const res = await fetch(`${process.env.BASE_API_URL}/buku-tamu`);
+    
+    if (!res.ok) {
+      throw new Error(`Failed to fetch data: ${res.statusText}`);
+    }
+
+    const data: GuestBookGetInstance = await res.json();
+
+    return {
+      props: {
+        data
+      }
+    };
+  } catch (error) {
+    console.error('Error fetching guest data:', error);
+    return {
+      props: {
+        data: null
+      }
+    };
+  }
+
 
 }
 
-const GuestBookPage: React.FC<GuestBookProps> = ({}) => {
+const GuestBookPage: React.FC<InferGetServerSidePropsType<typeof getGuestData>> = ({ data }) => {
   return (
     <DefaultLayout>
-        <GuestBook />
+      <GuestBook {...data} />
     </DefaultLayout>
-  )
-}
+  );
+};
 
 
 
