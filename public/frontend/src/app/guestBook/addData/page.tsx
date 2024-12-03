8552abfb0b05aa36d12620f779a7b7be7bbadc4c
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-12-03 14:43:22 +0700

    UPDATE: Changed data response type

--- public/frontend/src/app/guestBook/addData/page.tsx
+++ public/frontend/src/app/guestBook/addData/page.tsx
@@ -3,6 +3,7 @@
 import Breadcrumb from '@/components/Breadcrumbs/Breadcrumb'
 import InputFields from '@/components/Fields/InputFields'
 import DefaultLayout from '@/components/Layouts/DefaultLayout'
+import { GuestBookGetInstance } from '@/types/pages/guest'
 import Link from 'next/link'
 import React, { useState } from 'react'
 
@@ -20,15 +21,16 @@ interface dataProps {
 
 export default function GuestBookDetail() {
 
-    const [data, setData] = useState<dataProps[]>([{
+    const [data, setData] = useState<GuestBookGetInstance>({
         id: 0,
-        guestName: "",
-        instutionName: "",
-        instutionAddress: "",
-        needs: "",
-        position: "",
-        contact: ""
-    }])
+        nama_tamu: "",
+        alamat: "",
+        no_telpon: "",
+        institusi_tamu_id: 0,
+        divisi_id: 0,
+        keperluan: "",
+        user_id: 0,
+    })
 
 
 
@@ -50,35 +52,31 @@ export default function GuestBookDetail() {
                     />
                 </svg>
             </Link>
-            <Breadcrumb pageName="Detail Pengguna" />
+            <Breadcrumb pageName="Tambah Tamu" />
 
             <div className="flex flex-col gap-9 rounded-sm border border-stroke bg-white px-6.5 py-4 shadow-default dark:border-strokedark dark:bg-boxdark">
-                {data.map((data, index) => (
-                    <div key={index} className="grid grid-cols-2 gap-9">
-                        <InputFields name="Nama Tamu" defaultValue={data.guestName} />
-                        <InputFields
-                            name="Nama Instusi"
-                            defaultValue={data.instutionName}
-                        />
-                        <InputFields
-                            name="Alamat Instusi"
-                            defaultValue={data.instutionAddress}
-                        />
-                        <InputFields
-                            name="Keperluan"
-                            defaultValue={data.needs}
-                        />
-                        <InputFields
-                            name="Jabatan"
-                            defaultValue={data.position}
-                        />
-                        <InputFields
-                            name="Kontak"
-                            defaultValue={data.contact}
-                        />
+               
+                <div className="grid grid-cols-2 gap-9">
+                    <InputFields title="Nama Tamu" />
+                    <InputFields
+                        title="Alamat"
+                    />
+                    <InputFields
+                        title="No Telepon"
+                    />
+                    <InputFields
+                        title="Keperluan"
+                    />
+                    <InputFields
+                        title="Instansi Asal"
+                        autoCompleteData={["BlizbyteCo", "DISPORAPAR"]}
+                    />
+                    <InputFields
+                        title="Divisi Tujuan"
+                    />                    
 
-                    </div>
-                ))}
+                </div>
+
                 <button className="flex w-full justify-center rounded bg-primary p-3 font-medium text-gray hover:bg-opacity-90">
                     Tambahkan tamu
                 </button>
