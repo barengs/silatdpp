5473970396fa451f469adaa3c7c0e582ba3a1855
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-12-04 14:41:48 +0700

    UPDATE: Fixed fetch data

--- public/frontend/src/app/guestBook/page.tsx
+++ public/frontend/src/app/guestBook/page.tsx
@@ -1,42 +1,19 @@
 import DefaultLayout from '@/components/Layouts/DefaultLayout'
 import GuestBook from '@/pages/GuestBook/page'
-import { GuestBookGetInstance } from '@/types/pages/guest'
-import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
 import React from 'react'
 
 
-export const getGuestData: GetServerSideProps<{data: GuestBookGetInstance|null}> = async () => {
-
-  try {
-    const res = await fetch(`${process.env.BASE_API_URL}/buku-tamu`);
-    
-    if (!res.ok) {
-      throw new Error(`Failed to fetch data: ${res.statusText}`);
-    }
-
-    const data: GuestBookGetInstance = await res.json();
-
-    return {
-      props: {
-        data
-      }
-    };
-  } catch (error) {
-    console.error('Error fetching guest data:', error);
-    return {
-      props: {
-        data: null
-      }
-    };
-  }
 
 
-}
+const GuestBookPage: React.FC = async () => {
 
-const GuestBookPage: React.FC<InferGetServerSidePropsType<typeof getGuestData>> = ({ data }) => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API_URL}/buku-tamu`)
+  const data = await res.json()
+
+
   return (
     <DefaultLayout>
-      <GuestBook {...data} />
+      <GuestBook data={data.data.data} />
     </DefaultLayout>
   );
 };
