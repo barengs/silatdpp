7da71d5b4cdf4629f762dd3602c1eb18aabab686
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-12-04 14:43:49 +0700

    UPDATE: Changed to common type instead of defined type

--- public/frontend/src/pages/GuestBook/page.tsx
+++ public/frontend/src/pages/GuestBook/page.tsx
@@ -1,45 +1,44 @@
 "use client"
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import Breadcrumb from '@/components/Breadcrumbs/Breadcrumb'
 import Table from '@/components/Table/page'
 import Link from 'next/link'
-import { GuestBookGetInstance } from '@/types/pages/guest'
+import { GuestBookProps } from '@/types/pages/guest'
 
 
 
 
-const GuestBook: React.FC<GuestBookGetInstance> = (data) => {
+const GuestBook: React.FC<GuestBookProps> = ({data}) => {
 
   const columns = [
     {
       name: "Nama Tamu",
-      selector: (row: { nama_tamu: string }) => row.nama_tamu,
+      selector: (row: Record<string, string>) => row.nama_tamu,
       sortable: true,
     },
     {
       name: "Alamat",
-      selector: (row: { alamat: string }) => row.alamat,
+      selector: (row: Record<string, string>) => row.alamat,
       sortable: true,
     },
     {
       name: "No Telpon",
-      selector: (row: { no_telpon: string }) => row.no_telpon,
+      selector: (row: Record<string, string>) => row.no_telpon,
       sortable: true,
     },
     {
       name: "Keperluan",
-      selector: (row: { keperluan: string }) => row.keperluan,
+      selector: (row: Record<string, string>) => row.keperluan,
       sortable: true,
     },
     {
       name: "Aksi",
-      cell: (row: GuestBookGetInstance) => (
+      cell: (row: Record<string, string>) => (
         <Link className='text-blue-500 hover:underline' href={`/guestBook/${row.id}`}>Edit</Link>
       ),
     },
   ];
-
   
 
   // DEVELOPMENT PURPOSE
@@ -47,7 +46,7 @@ const GuestBook: React.FC<GuestBookGetInstance> = (data) => {
   return (
     <>
       <Breadcrumb pageName='Buku Tamu' />
-      <Table addButtonLink='/guestBook/addData' addButtonName='Tambah Tamu' name='Daftar Tamu' column={columns} data={data} detailLink={{name: "Pengaturan", to: "/guestBook"}}   />
+      <Table addButtonLink='/guestBook/addData' name='Daftar Tamu' column={columns} data={data} detailLink={{name: "Pengaturan", to: "/guestBook"}}   />
     </>
   )
 }
