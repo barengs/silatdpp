f44fddcee7adb5b1979354fa71d27449667c6404
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-12-04 14:44:07 +0700

    ADD: Institution data type

--- public/frontend/src/types/pages/guest.d.ts
+++ public/frontend/src/types/pages/guest.d.ts
@@ -3,14 +3,23 @@ export interface GuestBookGetInstance {
     nama_tamu: string,
     alamat: string,
     no_telpon: string,
-    institusi_tamu_id: 0,
-    divisi_id: 0,
+    institusi_tamu_id: string,
+    divisi_id: string,
     keperluan: string,
-    user_id: 0,
+    user_id: string,
 }
 
 
 
 export interface GuestBookProps {
     data: GuestBookGetInstance[]
+}
+
+export interface InstitutionsDataTypes {
+    id: number,
+    nama: string,
+    alamat: string,
+    kontak: string,
+    created_at: string,
+    updated_at: string
 }
