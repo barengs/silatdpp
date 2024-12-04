1f338046415843309251e335cfa5748b811e9572
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-12-04 14:42:16 +0700

    UPDATE: Added autocomplete to input field

--- public/frontend/src/components/Fields/InputFields.tsx
+++ public/frontend/src/components/Fields/InputFields.tsx
@@ -10,47 +10,62 @@ interface InputFieldsProps {
     autoCompleteData?: string[]
 }
 
-const InputFields = ({ title, defaultValue="", name="", autoCompleteData=[], placeholder=name }: InputFieldsProps) => {
+const InputFields = ({ title, autoCompleteData, defaultValue="", name="", placeholder=name }: InputFieldsProps) => {
 
     const [inputValue, setInputValue] = useState(defaultValue)
-    const [data, setData] = useState(autoCompleteData)
+    const [data, setData] = useState<string[]>([])
+    const [autoCompleteState, setAutoCompleteState] = useState(false)
 
     const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-        setInputValue(event.target.value.toLowerCase())
+
+        if (event.target.value) {
+            setAutoCompleteState(true)
+        } else {
+            setAutoCompleteState(false)
+        }
+
+        setInputValue(event.target.value)
+    }
+
+    const handleButtonClick = (data: string) => {
+        console.log(data)
+        setAutoCompleteState(false)
+        setInputValue(data)
     }
 
 
-    // useEffect(() => {
-        
-    //     if (!inputValue) return
-        
-    //     const res = data?.filter(name => {
-    //         return name.toLowerCase().includes(inputValue)
+    useEffect(() => {
+        setData(autoCompleteData)
+    }, [autoCompleteData])
 
-    //     })
 
-    //     setData(res)
-    // }, [inputValue])
+    const onBlurHandler = () => {
+        const delay = setTimeout(() => setAutoCompleteState(false), 500)
+        // clearTimeout(delay)
+    }
 
     return (
-        <div>
+        <div onBlur={onBlurHandler}>
+            {(autoCompleteState || inputValue) &&
+
             <label className="mb-3 block text-sm font-medium text-black dark:text-white">
                 {title}
             </label>
+            }
             <input
                 type="text"
-                placeholder={placeholder}
+                placeholder={title}
                 value={inputValue}
                 onChange={handleInputChange}
                 name={name}
-                className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent p-1.5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
             />
-            <div className={`rounded-md bg-gray-100 w-full overflow-hidden py-2 mt-2 ${ autoCompleteData ? '' : 'hidden'}`}>
+            <div className={`rounded-md bg-gray-100 w-full overflow-hidden py-2 mt-2 ${ autoCompleteState ? '' : 'hidden'}`}>
                 {data?.filter(name => {
                     if (!name) return true
-                    return name.toLowerCase().includes(inputValue)
+                    return name.toLowerCase().includes(inputValue.toLowerCase())
                 }).map((data, index) => (
-                    <button key={index} onClick={() => setInputValue(data)} className="hover:bg-blue-500 hover:text-white w-full">{data}</button>
+                    <button key={index} onClick={() => handleButtonClick(data)} className="hover:bg-blue-500 hover:text-white w-full">{data}</button>
                 ))}
             </div>
         </div>
