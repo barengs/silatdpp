e9c1627014854e90a0a3233ddabac552b32f1a1a
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-12-05 13:47:41 +0700

    ADD: Added Institution page

---
+++ public/frontend/src/app/institution/page.tsx
@@ -0,0 +1,22 @@
+import DefaultLayout from '@/components/Layouts/DefaultLayout'
+import InstutionsPage from '@/pages/Institution/page';
+import React from 'react'
+
+
+
+const Institution: React.FC = async () => {
+
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API_URL}/institusi-tamu`)
+  const data = await res.json()
+
+
+  return (
+    <DefaultLayout>
+      <InstutionsPage data={data.data.data} />
+    </DefaultLayout>
+  );
+};
+
+
+
+export default Institution
