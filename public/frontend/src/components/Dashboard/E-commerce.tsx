e07ea41c22b0aa403f765fba6670643159cac2e0
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-12-14 14:54:29 +0700

    ADD: Other page to card

--- public/frontend/src/components/Dashboard/E-commerce.tsx
+++ public/frontend/src/components/Dashboard/E-commerce.tsx
@@ -9,6 +9,8 @@ const ECommerce: React.FC = () => {
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
         <Card title="Buku Tamu" link="/guestBook"/>
         <Card title="Manajemen Pengguna" link="/users"/>
+        <Card title="Pengajuan SPPD" link="/sppd"/>
+        <Card title="Daftar Instansi" link="/institution"/>
       </div>
     </>
   );
