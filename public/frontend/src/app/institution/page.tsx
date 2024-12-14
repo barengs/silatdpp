7e6e071d22017b514f5f8233b7d9cdf4d697b15c
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-12-14 14:54:14 +0700

    UPDATE: Fixed unloaded institution data

--- public/frontend/src/app/institution/page.tsx
+++ public/frontend/src/app/institution/page.tsx
@@ -9,10 +9,12 @@ const Institution: React.FC = async () => {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API_URL}/institusi-tamu`)
   const data = await res.json()
 
+  console.log(data.data)
+
 
   return (
     <DefaultLayout>
-      <InstutionsPage data={data.data.data} />
+      <InstutionsPage data={data.data} />
     </DefaultLayout>
   );
 };
