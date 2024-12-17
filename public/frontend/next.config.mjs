42807755eadb770007bd5be721c8e02788df4829
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-12-17 17:23:17 +0700

    UPDATE: next config

--- public/frontend/next.config.mjs
+++ public/frontend/next.config.mjs
@@ -3,11 +3,11 @@ const nextConfig = {
     typescript: {
         ignoreBuildErrors: true
     },
-    output: 'export',
-    trailingSlash: true,
-    images: {
-        unoptimized: true
-    }
+    // output: 'export',
+    // trailingSlash: true,
+    // images: {
+    //     unoptimized: true
+    // }
 };
 
 export default nextConfig;
