36bc61f9ff2cef8167db53de8effbf1c30684607
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-12-20 20:24:38 +0700

    ADD: auth for checking token validation in every page

--- public/frontend/src/app/sppd/addData/page.tsx
+++ public/frontend/src/app/sppd/addData/page.tsx
@@ -3,9 +3,11 @@
 import Breadcrumb from "@/components/Breadcrumb";
 import FilesFields from "@/components/Fields/FileFields";
 import InputFields from "@/components/Fields/InputFields";
+import ListFields from "@/components/Fields/ListField";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
+import { useStore } from "react-redux";
 
 
 
@@ -13,10 +15,12 @@ const SppdAddData: React.FC = () => {
 
     const [postData, setPostData] = useState<Record<string, unknown>>({
         file: [],
-        user_id: 1
     })
 
     const router = useRouter()
+
+    const store = useStore()
+    const state = store.getState().auth
 
     const handleDataChange = (name: string, value: unknown) => {
         setPostData(prevState => {
@@ -30,11 +34,13 @@ const SppdAddData: React.FC = () => {
 
         Object.keys(postData).map(keyName => formData.append(keyName, postData[keyName]))
 
+        formData.append("user_id", state.userId)
+
         const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API_URL}/sppd`, {
             method: 'post',
             body: formData,
             headers: {
-                Authorization: `Bearer ${process.env.NEXT_PUBLIC_TEMP_CREDENTIAL}`
+                Authorization: `Bearer ${state.token}`
             }
         })
 
@@ -56,8 +62,12 @@ const SppdAddData: React.FC = () => {
             <div className="flex flex-col gap-9 rounded-sm border border-stroke bg-white px-6.5 py-4 shadow-default dark:border-strokedark dark:bg-boxdark">
                 <InputFields title="Nama Kegiatan" onValueChange={(value: string) => handleDataChange("nama_kegiatan", value)} />
                 <InputFields title="Tempat Kegiatan" onValueChange={(value: string) => handleDataChange("tempat_kegiatan", value)} />
-                <InputFields title="Tanggal Kegiatan" onValueChange={(value: string) => handleDataChange("tanggal_kegiatan", value)}type="date"/>
+                <div className="flex gap-x-4">
+                    <InputFields title="Tanggal Berangkat" onValueChange={(value: string) => handleDataChange("tanggal_kegiatan", value)}type="date"/>
+                    <InputFields title="Tanggal Sampai" onValueChange={(value: string) => handleDataChange("tanggal_kegiatan", value)}type="date"/>
+                </div>
                 <FilesFields setter={(files: File[]) => handleDataChange("files", JSON.stringify(files))} title="Bukti Kegiatan" />
+                <ListFields title="Peserta Perjalanan" addText="Tambah Peserta" dataURL="" />
                 <button
                     onClick={handlePostData}
                     className="flex w-full justify-center rounded bg-primary p-3 font-medium text-gray hover:bg-opacity-90"
