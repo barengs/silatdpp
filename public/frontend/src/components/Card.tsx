a0a2210a9499c2afad38cf301de7776179e27bfb
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-12-26 03:45:46 +0700

    UPDATE: Added redirection onClick to the Card element

--- public/frontend/src/components/Card.tsx
+++ public/frontend/src/components/Card.tsx
@@ -9,10 +9,9 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({title, link}) => {
   return (
-    <div className='rounded-md bg-white p-4'>
+    <Link href={link} className='rounded-md bg-white p-4'>
         <h3 className='text-black font-bold text-lg mb-4'>{title}</h3>
-        <Link href={link} className='text-sm hover:underline'>To page</Link>
-    </div>
+    </Link>
   )
 }
 
