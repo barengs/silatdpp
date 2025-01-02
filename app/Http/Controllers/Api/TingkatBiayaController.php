15f05f4f5e979f4f9235b4302b3d1ce8753c3480
rofiareiv <kambing.rofi@gmail.com>
2025-01-02 19:58:11 +0700

    fixed bug validator

--- app/Http/Controllers/Api/TingkatBiayaController.php
+++ app/Http/Controllers/Api/TingkatBiayaController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
-use App\Http\Resources\ApiResource;
 use App\Models\TingkatBiaya;
 use Illuminate\Http\Request;
+use App\Http\Resources\ApiResource;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 
 class TingkatBiayaController extends Controller
 {
