1637620be5aee79052006620f11bfd83a36c151a
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-01-03 17:41:39 +0700

    ADD: Added budget pge

---
+++ public/frontend/src/pages/Budget/page.tsx
@@ -0,0 +1,12 @@
+import Table from "@/components/Table"
+
+const BudgetPage: React.FC = () => {
+    return (
+        <>
+            <Table data={[]} column={[]} name="Budget" addButtonLink="" addButtonName="Tambah Budget" />
+        </>
+    )
+}
+
+
+export default BudgetPage
