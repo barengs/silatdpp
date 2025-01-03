7c3607f80eadfd30fe7dc399d85dc28ea8a27a22
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-01-03 17:35:25 +0700

    UPDATE: implement redux-persist

--- public/frontend/src/app/layout.tsx
+++ public/frontend/src/app/layout.tsx
@@ -5,7 +5,9 @@ import "@/css/style.css";
 import React, { useEffect, useState } from "react";
 import Loader from "@/components/Loader";
 import { Provider } from "react-redux";
-import { store } from "@/store";
+import { persistor, store } from "@/store";
+import { PersistGate } from "redux-persist/integration/react";
+
 
 export default function RootLayout({
   children,
@@ -14,10 +16,11 @@ export default function RootLayout({
 }>) {
   const [loading, setLoading] = useState<boolean>(true);
 
-  // const pathname = usePathname();
 
   useEffect(() => {
-    setTimeout(() => setLoading(false), 1000);
+    const timeout = setTimeout(() => setLoading(false), 1000);
+
+    return () => clearTimeout(timeout)
   }, []);
 
   return (
@@ -25,7 +28,9 @@ export default function RootLayout({
       <body suppressHydrationWarning={true}>
         <div className="dark:bg-boxdark-2 dark:text-bodydark">
           <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
               {loading ? <Loader /> : children}
+            </PersistGate>
           </Provider>
         </div>
       </body>
