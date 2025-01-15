d5d45928e321ccb0791b19a65898a3df4069bc85
rofiareiv <kambing.rofi@gmail.com>
2025-01-15 14:04:10 +0700

    change Rekanan and Rekom model

--- app/Models/Rekom.php
+++ app/Models/Rekom.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Rekom extends Model
 {
-    //
+    protected $fillables = [
+        'regno',
+        'user_id',
+        'rekanan_id',
+        'institusi_id',
+        'nama_pejabat',
+        'nip_pejabat',
+        'nama_pejabat_pengganti',
+        'nip_pejabat_pengganti',
+        'alamat_pejabat_pengganti',
+        'jabatan',
+        'konten',
+        'status',
+    ];
 }
