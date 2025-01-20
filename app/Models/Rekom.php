7eb3a20486264f3143e554b32294b0977939bcd3
rofiareiv <kambing.rofi@gmail.com>
2025-01-20 14:13:45 +0700

    fixed on production

--- app/Models/Rekom.php
+++ app/Models/Rekom.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Rekom extends Model
 {
-    protected $fillables = [
+    protected $fillable = [
         'regno',
         'user_id',
         'rekanan_id',
