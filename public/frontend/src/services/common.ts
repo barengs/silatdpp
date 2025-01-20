86c05ce9ea8c4efbf6d1e4755b1cffe2714c134a
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-01-20 15:43:54 +0700

    ADD: Added service to get partner data

--- public/frontend/src/services/common.ts
+++ public/frontend/src/services/common.ts
@@ -1,7 +1,7 @@
-import { DEFAULT_DIVISION_DATA, GUEST_BOOK_DEFAULT_DATA, INSTITUTION_DEFAULT_DATA } from "@/utils/constans"
+import { DEFAULT_DIVISION_DATA, DEFAULT_PARTNERS_DATA, GUEST_BOOK_DEFAULT_DATA, INSTITUTION_DEFAULT_DATA } from "@/utils/constans"
 
 export async function fetchInsitution() {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API_URL}/institusi-tamu`)
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API_URL}/institusi`)
 
     if (!res.ok) return [INSTITUTION_DEFAULT_DATA]
 
@@ -26,6 +26,16 @@ export async function fetchDivision() {
     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API_URL}/divisi`)
 
     if (!res.ok) return [DEFAULT_DIVISION_DATA]
+
+    const data = await res.json()
+
+    return data.data
+}
+
+export async function fetchPartners() {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API_URL}/rekanan`)
+
+    if (!res.ok) return [DEFAULT_PARTNERS_DATA]
 
     const data = await res.json()
 
