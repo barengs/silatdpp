e6133852a679dd4e99835cd3f3a40dca501ce479
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-01-26 17:15:08 +0700

    UPDATE: Re-Layouting Sidebar features

--- public/frontend/src/components/Sidebar/SidebarItem.tsx
+++ public/frontend/src/components/Sidebar/SidebarItem.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import SidebarDropdown from "@/components/Sidebar/SidebarDropdown";
 import { usePathname } from "next/navigation";
 
-const SidebarItem = ({ item, pageName, setPageName }: any) => {
+const SidebarItem = ({ item, pageName, setPageName, sidebarOpened }: any) => {
   const handleClick = () => {
     const updatedPageName =
       pageName !== item.label.toLowerCase() ? item.label.toLowerCase() : "";
@@ -28,11 +28,11 @@ const SidebarItem = ({ item, pageName, setPageName }: any) => {
         <Link
           href={item.route}
           onClick={handleClick}
-          className={`${isItemActive ? "bg-graydark dark:bg-meta-4" : ""} group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4`}
+          className={`${isItemActive ? "bg-graydark dark:bg-meta-4" : ""} group relative flex items-center gap-2.5 rounded-sm py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4`}
         >
           {item.icon}
-          {item.label}
-          {item.children && (
+            {sidebarOpened ? item.label : ''}
+          {(item.children && sidebarOpened) && (
             <svg
               className={`absolute right-4 top-1/2 -translate-y-1/2 fill-current ${
                 pageName === item.label.toLowerCase() && "rotate-180"
@@ -55,7 +55,7 @@ const SidebarItem = ({ item, pageName, setPageName }: any) => {
 
         {item.children && (
           <div
-            className={`translate transform overflow-hidden ${
+            className={`overflow-hidden ${
               pageName !== item.label.toLowerCase() && "hidden"
             }`}
           >
--- public/frontend/src/components/Sidebar/index.tsx
+++ public/frontend/src/components/Sidebar/index.tsx
@@ -53,6 +53,10 @@ const menuGroups = [
                 ),
                 label: "Buku Tamu",
                 route: "/guestBook",
+                children: [
+                    { label: "Registrasi Tamu", route: "/guestBook" },
+                    { label: "List Tamu", route: "/guestBook/list" },
+                ],
             },
             {
                 icon: (
@@ -76,7 +80,13 @@ const menuGroups = [
                 ),
                 label: "Master",
                 route: "#",
-                children: [{ label: "Manajemen Pengguna", route: "/users" }],
+                children: [
+                    { label: "Data Divisi", route: "/division" },
+                    { label: "Data Instansi", route: "/institution" },
+                    { label: "Data Transportasi", route: "/transportation" },
+                    { label: "Data Biaya", route: "/budget" },
+                    { label: "Data Rekanan", route: "/partners" },
+                ],
             },
             {
                 icon: (
@@ -95,12 +105,9 @@ const menuGroups = [
                         />
                     </svg>
                 ),
-                label: "Manajemen Berkas",
+                label: "Sppd",
                 route: "#",
-                children: [
-                    { label: "Pengajuan Sppd", route: "/sppd" },
-                    { label: "Pengajuan Rekomendasi", route: "/recomendation" },
-                ],
+                children: [{ label: "Pengajuan Sppd", route: "/sppd" }],
             },
             {
                 icon: (
@@ -115,17 +122,44 @@ const menuGroups = [
                         <path
                             strokeLinecap="round"
                             strokeLinejoin="round"
-                            d="M3.75 21h16.5M4.5 3h15M5.25 3v18m13.5-18v18M9 6.75h1.5m-1.5 3h1.5m-1.5 3h1.5m3-6H15m-1.5 3H15m-1.5 3H15M9 21v-3.375c0-.621.504-1.125 1.125-1.125h3.75c.621 0 1.125.504 1.125 1.125V21"
+                            d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
                         />
                     </svg>
                 ),
-                label: "Manajemen Data",
+                label: "Rekomendasi",
                 route: "#",
                 children: [
-                    { label: "Data Instansi", route: "/institution" },
-                    { label: "Data Biaya", route: "/budget" },
-                    { label: "Data Transportasi", route: "/transportation" },
-                    { label: "Data Rekanan", route: "/partners" },
+                    { label: "Penggantian Bendahara", route: "/recomendation" }
+                ],
+            },
+            {
+                icon: (
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="size-6 shrink-1"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z"
+                        />
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                        />
+                    </svg>
+                ),
+                label: "Pengaturan Pengguna",
+                route: "#",
+                children: [
+                    { label: "Tambah Karyawan", route: "/users" },
+                    { label: "Tugas", route: "/institution" },
+                    { label: "Hak Akses", route: "/institution" },
                 ],
             },
         ],
@@ -185,6 +219,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                                         item={menuItem}
                                         pageName={pageName}
                                         setPageName={setPageName}
+                                        sidebarOpened={sidebarOpen}
                                     />
                                 ))}
                             </ul>
