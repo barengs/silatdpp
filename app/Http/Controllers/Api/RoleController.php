d6a5674747bee5a1310a69a5e860abaf08839b6b
rofiareiv <kambing.rofi@gmail.com>
2025-01-31 19:51:40 +0700

    add role and permission controller

---
+++ app/Http/Controllers/Api/RoleController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+use App\Http\Resources\ApiResource;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+
+class RoleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $roles = Role::all();
+        return new ApiResource(true, 'daftar data role', $roles);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $role = Role::create([
+            'name' => $request->name,
+            'guard_name' => $request->guard == '' ? 'web' : $request->guard,
+        ]);
+
+        return new ApiResource(true, 'Tugas berhasil di tambahkan!', $role);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
