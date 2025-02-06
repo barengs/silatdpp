2d94c50540c5693e48b12faba0e9ceb892ae51af
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-02-06 09:31:49 +0700

    UPDATE: Added state sync to user list page

--- public/frontend/src/app/users/list/page.tsx
+++ public/frontend/src/app/users/list/page.tsx
@@ -1,34 +1,62 @@
-"use client"
+"use client";
 
 import Breadcrumb from "@/components/Breadcrumb";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import Table from "@/components/Table";
-import { useState } from "react";
+import { fetchUsers } from "@/services/common";
+import { setUsers } from "@/store/servicesSlice";
+import Link from "next/link";
+import { useEffect } from "react";
+import { useDispatch, useStore } from "react-redux";
 
 const Page: React.FC = () => {
-
-    const [data, setData] = useState([])
+    const dispatch = useDispatch();
+    const store = useStore();
+    const serviceState = store.getState().services;
 
     const columns = [
         {
-            name: "Nama",
-            selector: (row: Record<string, string>) => row.nama,
+            name: "Nama Karyawan",
+            selector: (row) => row.name,
             sortable: true,
         },
         {
-            name: "Email",
-            selector: (row: Record<string, string>) => row.email,
+            name: "Email Karyawan",
+            selector: (row) => row.email,
             sortable: true,
         },
-    ]
+        {
+            name: "Aksi",
+            cell: (row: Record<string, string>) => (
+                <Link
+                    className="text-blue-500 hover:underline"
+                    href={`/users/${row.id}`}
+                >
+                    Edit
+                </Link>
+            ),
+        },
+    ];
+
+    useEffect(() => {
+        const syncUserData = async () => {
+            dispatch(setUsers(await fetchUsers()));
+        };
+
+        syncUserData();
+    }, []);
 
     return (
         <DefaultLayout>
-            <Breadcrumb pageName="List Pengguna" />
-            <Table columns={columns} data={data} name="Data Pengguna" addButtonLink="" addButtonName="Tambah Pengguna"/>
+            <Breadcrumb pageName="Daftar Karyawan" />
+            <Table
+                column={columns}
+                data={serviceState.users}
+                addButtonName="Tambah Karyawan"
+                addButtonLink="/register"
+            />
         </DefaultLayout>
-    )
-}
-
+    );
+};
 
 export default Page;
