d57040597e6719d8a2b55bc279c386d3980af272
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-02-06 09:29:39 +0700

    UPDATE: Using useFetch hooks to display loading

--- public/frontend/src/app/login/page.tsx
+++ public/frontend/src/app/login/page.tsx
@@ -7,18 +7,20 @@ import { setToken } from "@/store/authSlice"
 import { fetchBudget, fetchDivision, fetchGuestBook, fetchInsitution, fetchPartners, fetchTransportation } from "@/services/common"
 import { setBudget, setDivision, setGuestBook, setInstitution, setPartners, setTransportation } from "@/store/servicesSlice"
 import { toast } from "react-toastify"
+import useFetch from "@/hooks/useFetch"
 
 
 export default function Page() {
   const router = useRouter()
   const dispatch = useDispatch()
+  const [isPending, fetchCaller] = useFetch()
 
   const onSubmit = async (event: React.FormEvent) => {
       event.preventDefault()
   
       const formData = new FormData(event.target)
 
-      const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API_URL}/login`, { 
+      const res = await fetchCaller('login', { 
           method: "POST",
           body: formData
       })
@@ -59,8 +61,20 @@ export default function Page() {
                   <h3 className="mb-2 font-medium text-black">Password</h3>
                   <input name="password" type="password" className="w-[400px] py-2 px-2 rounded-md border-[1.5px] border-gray-200"/>
               </div>
-              <button type="submit" className="w-full py-2 bg-primary
-               text-white rounded-md">Login</button>
+              <button
+                    className="flex w-full items-center justify-center gap-x-2 rounded bg-primary p-3 font-medium text-gray hover:bg-opacity-90 disabled:bg-opacity-75 disabled:cursor-not-allowed"
+                    type="submit"
+                    disabled={isPending}
+                >
+                {isPending ? (
+                        <>
+                            <div className="size-4 animate-spin rounded-full border-2 border-white border-t-transparent"></div>
+                            Memeriksa identitas
+                        </>
+                    ) : (
+                        <>Masuk</>
+                    )}
+               </button>
               </form>
           </div>
       </div>
