c68669ffa85e8b92f79bfe7ee3c38cb49a0d9942
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-02-06 09:29:06 +0700

    UPDATE: Added icon and description to card element

--- public/frontend/src/components/Card.tsx
+++ public/frontend/src/components/Card.tsx
@@ -3,14 +3,20 @@ import React from 'react'
 
 interface CardProps {
     title: string,
-    link: string
+    link: string,
+    icon?: React.ReactElement<any, any>,
+    desc?: string
 }
 
 
-const Card: React.FC<CardProps> = ({title, link}) => {
+const Card: React.FC<CardProps> = ({title, link, icon, desc}) => {
   return (
-    <Link href={link} className='rounded-md bg-white p-4'>
-        <h3 className='text-black font-bold text-lg mb-4'>{title}</h3>
+    <Link href={link} className='rounded-md bg-white p-4 flex gap-x-6 items-center'>
+        {icon}
+        <div className='flex flex-col'>
+          <h3 className='text-black font-bold text-lg'>{title}</h3>
+          <p className='text-xs'>{desc}</p>
+        </div>
     </Link>
   )
 }
