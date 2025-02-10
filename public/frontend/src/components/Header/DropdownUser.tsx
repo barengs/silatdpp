979e02357c591bf8ca6c4ed355e81c85346858cb
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-02-10 17:05:12 +0700

    ONCE AGAIN AND AGAIN FIXED UNDEFINED DATA OF USER

--- public/frontend/src/components/Header/DropdownUser.tsx
+++ public/frontend/src/components/Header/DropdownUser.tsx
@@ -58,9 +58,9 @@ const DropdownUser = () => {
             >
                 <span className="hidden text-right lg:block">
                     <span className="block text-sm font-medium text-black dark:text-white">
-                        {userData.name}
+                        {userData ? userData.name : ''}
                     </span>
-                    <span className="block text-xs">{userData.roles[0].name}</span>
+                    <span className="block text-xs">{userData ? userData.roles[0].name : ''}</span>
                 </span>
 
                 <span className="h-12 w-12 rounded-full">
