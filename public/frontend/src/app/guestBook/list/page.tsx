1d52764672461eb37676891b147f8fce07482efe
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-02-11 13:13:56 +0700

    UPDATE: Added edit modal to guest book list data

--- public/frontend/src/app/guestBook/list/page.tsx
+++ public/frontend/src/app/guestBook/list/page.tsx
@@ -3,52 +3,64 @@
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import Table from "@/components/Table";
 import { fetchGuestBook } from "@/services/common";
-import Link from "next/link";
-import React, { useEffect } from "react";
+import { GUEST_BOOK_DEFAULT_DATA } from "@/utils/constans";
+import Modal from "@/components/Modal";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useStore } from "react-redux";
+import InputFields from "@/components/Fields/InputFields";
+import { setGuestBook } from "@/store/servicesSlice";
 
-const GuestBookPage: React.FC = async () => {
+const GuestBookPage: React.FC = () => {
     const dispatch = useDispatch();
     const store = useStore();
-    const serviceState = store.getState().services;
+
+    const [showPopup, setShowPopup] = useState(false);
+    const [selectedData, setSelectedData] = useState(GUEST_BOOK_DEFAULT_DATA);
+    const [data, setData] = useState(store.getState().services.guestBook)
+
+    const handleSelectedData = (data) => {
+        setShowPopup(true);
+        setSelectedData(data);
+    };
 
     const columns = [
-      {
-          name: "Nama Tamu",
-          selector: (row: Record<string, string>) => row.nama_tamu,
-          sortable: true,
-      },
-      {
-          name: "Alamat",
-          selector: (row: Record<string, string>) => row.alamat,
-          sortable: true,
-      },
-      {
-          name: "No Telpon",
-          selector: (row: Record<string, string>) => row.no_telpon,
-          sortable: true,
-      },
-      {
-          name: "Keperluan",
-          selector: (row: Record<string, string>) => row.keperluan,
-          sortable: true,
-      },
-      {
-          name: "Aksi",
-          cell: (row: Record<string, string>) => (
-              <Link
-                  className="text-blue-500 hover:underline"
-                  href={`/guestBook/${row.id}`}
-              >
-                  Edit
-              </Link>
-          ),
-      },
-  ];
+        {
+            name: "Nama Tamu",
+            selector: (row: Record<string, string>) => row.nama_tamu,
+            sortable: true,
+        },
+        {
+            name: "Alamat",
+            selector: (row: Record<string, string>) => row.alamat,
+            sortable: true,
+        },
+        {
+            name: "No Telpon",
+            selector: (row: Record<string, string>) => row.no_telpon,
+            sortable: true,
+        },
+        {
+            name: "Keperluan",
+            selector: (row: Record<string, string>) => row.keperluan,
+            sortable: true,
+        },
+        {
+            name: "Aksi",
+            cell: (row: Record<string, string>) => (
+                <button
+                    className="text-blue-500 hover:underline"
+                    onClick={() => handleSelectedData(row)}
+                >
+                    Edit
+                </button>
+            ),
+        },
+    ];
 
     useEffect(() => {
         const syncGuestBookData = async () => {
-            dispatch(await fetchGuestBook());
+            dispatch(setGuestBook(await fetchGuestBook()));
+            setData(store.getState().services.guestBook);
         };
 
         syncGuestBookData();
@@ -56,12 +68,12 @@ const GuestBookPage: React.FC = async () => {
 
     return (
         <DefaultLayout>
-          <Table
+            <Table
                 addButtonName="Tambah Tamu"
                 addButtonLink="/guestBook"
                 name="Daftar Tamu"
                 column={columns}
-                data={serviceState.guestBook}
+                data={data}
                 detailLink={{ name: "Pengaturan", to: "/guestBook" }}
                 excludes={[
                     "id",
@@ -73,7 +85,33 @@ const GuestBookPage: React.FC = async () => {
                     "user_id",
                 ]}
             />
-
+            <Modal
+                url={`buku-tamu/${selectedData.id}`}
+                title="Edit Divisi"
+                state={showPopup}
+                stateSetter={setShowPopup}
+            >
+                <InputFields
+                    title="Nama Tamu"
+                    name="nama"
+                    defaultValue={selectedData.nama_tamu}
+                />
+                <InputFields
+                    title="Alamat Tamu"
+                    name="alamat"
+                    defaultValue={selectedData.alamat}
+                />
+                <InputFields
+                    title="No Telepon"
+                    name="Kontak"
+                    defaultValue={selectedData.no_telpon}
+                />
+                <InputFields
+                    title="Keperluan"
+                    name="Kontak"
+                    defaultValue={selectedData.keperluan}
+                />
+            </Modal>
         </DefaultLayout>
     );
 };
