882d75ac7b961faf7a60f4781155489ff56b2a00
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-02-14 08:36:09 +0700

    UPDATE: Fixed SPPD list page

--- public/frontend/src/app/sppd/list/page.tsx
+++ public/frontend/src/app/sppd/list/page.tsx
@@ -1,29 +1,23 @@
 "use client"
 
-import Table from "@/components/Table"
-import { SppdDataType } from "@/types/pages/sppd"
 import React, { useEffect, useState } from "react"
+import { useDispatch, useStore } from "react-redux"
+import Table from "@/components/Table"
+import { setSppd } from "@/store/servicesSlice"
+import { SppdDataType } from "@/types/pages/sppd"
 import { getDateTime } from "@/utils/data"
 import { DEFAULT_SPPD_DATA } from "@/utils/constans"
-import useFetch from "@/hooks/useFetch"
-import { toast } from "react-toastify"
 import DefaultLayout from "@/components/Layouts/DefaultLayout"
 import Breadcrumb from "@/components/Breadcrumb"
-import { useDispatch, useStore } from "react-redux"
-import { setSppd } from "@/store/servicesSlice"
 import { fetchSppd } from "@/services/common"
 import Modal from "@/components/Modal";
 import InputFields from "@/components/Fields/InputFields"
 import SelectFields from "@/components/Fields/SelectFields"
-import FilesFields from "@/components/Fields/FileFields"
 import TextFields from "@/components/Fields/TextFields"
-
 
 const SppdPage: React.FC = () => {
     const store = useStore();
     const dispatch = useDispatch()
-
-    const [_, fetchCaller] = useFetch()
     const [data, setData] = useState(store.getState().services.sppd)
     const [showPopup, setShowPopup] = useState(false);
     const [selectedData, setSelectedData] = useState(DEFAULT_SPPD_DATA);
@@ -36,10 +30,11 @@ const SppdPage: React.FC = () => {
      useEffect(() => {
             const syncState = async () => {
                 dispatch(setSppd(await fetchSppd()));
-                setData(setSppd(await fetchSppd()));
+                setData(store.getState().services.sppd);
             };
     
             syncState();
+
         }, []);
     
 
@@ -82,69 +77,69 @@ const SppdPage: React.FC = () => {
     ]
 
 
-    useEffect(() => {
-        const getData = async() => {
-            await fetchCaller('sppd')
-                .then(res => {
-                    if (res.ok) {
-                        return res.json()
-                    }
-
-                    toast.error("Galat saat mengambil data sppd", {
-                        position: 'top-right'
-                    })
-                    console.log(res)
-                })
-                .then(data => data ? setData(data.data.data) : [DEFAULT_SPPD_DATA])
-        }
-
-        getData()
-    }, [])
-
     return (
         <DefaultLayout>
             <Breadcrumb pageName="Histori SPPD" />
             <Table addButtonName='Tambah SPPD' addButtonLink='/sppd' name='Daftar Pengajuan SPPD' column={columns} data={data} detailLink={{name: "Pengaturan", to: "/sppd"}}   />
             <Modal 
                 url={`buku-tamu/${selectedData.id}`}
-                title="Edit Divisi"
+                title="Detail SPPD"
                 state={showPopup}
                 stateSetter={setShowPopup}
-                ableDelete={true}
             >
-                <InputFields title="Nama Kegiatan" name="nama_kegiatan" />
-                <InputFields title="Tempat Tujuan" name="tempat_tujuan" />
-                <InputFields title="Tempat Kegiatan" name="tempat_kegiatan" />
-                <InputFields title="Tempat Berangkat" name="tempat_berangkat" />
-                <TextFields title="Deskripsi Kegiatan" name="maksud_kegiatan" />
-                <SelectFields
+                <InputFields title="Tempat Tujuan" name="tempat_tujuan" defaultValue={selectedData.tempat_tujuan} disabled={true}/>
+                <InputFields title="Tempat Berangkat" name="tempat_berangkat" defaultValue={selectedData.tempat_berangkat} disabled={true}/>
+                <InputFields title="Status Persetujuan" name="approval" defaultValue={selectedData.approval ? selectedData.approval : 'Belum Disetujui'} disabled={true}/>
+                <InputFields title="Maksud Kegiatan" name="maksud_kegiatan" defaultValue={selectedData.maksud_kegiatan} disabled={true}/>
+                <InputFields
                     title="Transportasi Perjalanan"
                     name="alat_transportasi_id"
-                    options={[]}
+                    disabled={true}
+                    defaultValue={selectedData.alat_transportasi_id}
                 />
                 <div className="flex gap-x-4">
                     <InputFields
                         title="Tanggal Berangkat"
                         name="tanggal_berangkat"
-                        type="date"
+                        disabled={true}
+                        defaultValue={selectedData.tanggal_berangkat}
                     />
                     <InputFields
                         title="Tanggal Sampai"
                         name="tanggal_kembali"
-                        type="date"
+                        disabled={true}
+                        defaultValue={selectedData.tanggal_kembali}
                     />
                 </div>
                 <InputFields
                     title="Tanggal Kegiatan"
                     name="tanggal_kegiatan"
-                    type="date"
+                    disabled={true}
+                    defaultValue={selectedData.tanggal_kegiatan}
                 />
                
-                <SelectFields
+                <InputFields
                     title="Biaya Perjalanan"
                     name="tingkat_biaya_id"
-                    options={[]}
+                    disabled={true}
+                    defaultValue={selectedData.tingkat_biaya_id}
                 />
+                <InputFields
+                    title="Status Diterima"
+                    name="tingkat_biaya_id"
+                    disabled={true}
+                    defaultValue={selectedData.approval ? "Disetujui" : "Belum Disetujui"}
+                />
+                <div className="col-span-2 text-black-2">
+                    <h2>Histori Alur</h2>
+                    {selectedData.history.map(history => (
+                        <div className="mt-4 px-2">
+                            <h3 className="font-semibold">{history.nama}</h3>
+                            <p className="text-sm text-gray-500">{getDateTime(history.created_at)}</p>
+                        </div>
+                    ))}
+                </div>
+
             </Modal>
         </DefaultLayout>
     )
