c278a8355224e564e3d928fabac5811aa96503fb
rofiareiv <kambing.rofi@gmail.com>
2025-02-17 18:48:46 +0700

    add module ijazah

---
+++ app/Http/Controllers/Api/IjazahConroller.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Ijazah;
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use App\Http\Resources\ApiResource;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+
+class IjazahConroller extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $data = Ijazah::latest()->get();
+
+        return new ApiResource(true, 'data pengajuan perubahan ijazah', $data);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validasi = Validator::make($request->all(), [
+            'institusi_id' => 'required',
+            'nama_siswa' => 'required',
+            'nis' => 'required',
+            'perubahan' => 'required',
+            'alasan' => 'required',
+            'file' => 'required',
+        ]);
+
+        if ($validasi->fails()) {
+            return response()->json($validasi->errors(), 422);
+        }
+
+        $user = JWTAuth::parseToken()->authenticate();
+
+        if ($request->hasFile('file')) {
+            $file = $request->file('file');
+            $fileName = time() . '-' . $file->getClientOriginalName();
+            $filePath = $file->move('documents/ijazah', $fileName);
+        }
+
+        $save = Ijazah::create([
+            'institusi_id' => $request->institusi_id,
+            'nama_siswa' => $request->nama_siswa,
+            'nis' => $request->nis,
+            'keperluan' => $request->keperluan,
+            'alasan' => $request->alasan,
+            'file' => $fileName,
+            'user_id' => $user->id,
+        ]);
+
+        if ($save) {
+            return new ApiResource(true, 'pengajuan berhasil disimpan', $save);
+        } else {
+            return new ApiResource(false, 'gagal membuat pengajuan', '');
+        }
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $data = Ijazah::findOrFail($id);
+        return new ApiResource(true, 'detil pengajuan', $data);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $ajuan = Ijazah::findOrFail($id);
+        $ajuan->institusi_id = $request->institusi_id;
+        $ajuan->nama_siswa = $request->nama_siswa;
+        $ajuan->nis = $request->nis;
+        $ajuan->keperluan = $request->keperluan;
+        $ajuan->alasan = $request->alasan;
+        $ajuan->status = $request->status;
+
+        $ajuan->update();
+
+        return new ApiResource(true, 'berhasil merubah pengajuan', $ajuan);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
