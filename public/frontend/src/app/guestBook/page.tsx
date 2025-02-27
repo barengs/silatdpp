4069faeabd82fe02ce0a7e4420f196a930110c7b
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-02-27 20:48:52 +0700

    UPDATE: Added validation to guest book page

--- public/frontend/src/app/guestBook/page.tsx
+++ public/frontend/src/app/guestBook/page.tsx
@@ -12,10 +12,12 @@ import { toast } from "react-toastify";
 import useFetch from "@/hooks/useFetch";
 import { fetchGuestBook } from "@/services/common";
 import { setGuestBook } from "@/store/servicesSlice";
+import { z } from "zod";
 
 export default function GuestBookDetail() {
     const dispatch = useDispatch()
     const [isPending, fetchCaller] = useFetch();
+    const [errors, setErrors] = useState({});
     const state = useStore().getState();
     const authState = state.auth;
     const servicesState = state.services;
@@ -23,6 +25,17 @@ export default function GuestBookDetail() {
     const [lastGuest, _] = useState(servicesState.guestBook[0] || null)
 
     const [selectedInstitution, setSelectedInstitution] = useState("");
+
+    const schema = z.object({
+            nama_tamu: z.string().min(1, "Nama tidak boleh kosong!"),
+            alamat: z.string().min(1, "Alamat tidak boleh kosong!"),
+            no_telepon: z.number({ invalid_type_error: "Nomor telepon tidak valid"}).min(1, "No Telepon tidak boleh kosong!"),
+            keperluan: z.string().min(1, "Keperluan tidak boleh kosong!"),
+            institusi_id: z.string().min(1, "Institusi tidak boleh kosong!"),
+            alamat_institusi: z.string().min(1, "Alamat Institusi tidak boleh kosong!"),
+            kontak_institusi: z.string().min(1, "Kontak Institusi tidak boleh kosong!"),
+        });
+    
 
     const syncGuestBookData = async() => {
         dispatch(setGuestBook(await fetchGuestBook()))
@@ -50,6 +63,23 @@ export default function GuestBookDetail() {
         const form = event.currentTarget;
         const data = new FormData(form);
         const currentInstitution = data.get("institusi_id");
+
+        const res = schema
+            .safeParse({ 
+                nama_tamu: data.get("nama_tamu"),
+                alamat: data.get("alamat"),
+                no_telepon: data.get("no_telepon"),
+                keperluan: data.get("keperluan"),
+                institusi_id: data.get("institusi_id"),
+                alamat_institusi: data.get("alamat_institusi"),
+                kontak_institusi: data.get("kontak_institusi"),
+            })
+            .error?.flatten().fieldErrors;
+
+        if (Object.keys(res).length >= 1) {
+            setErrors(res);
+            return;
+        }
     
         servicesState.institutions.forEach((institution) => {
             if (institution.nama === currentInstitution) {
@@ -99,10 +129,10 @@ export default function GuestBookDetail() {
                     onSubmit={handlePostData}
                     className="grid grid-cols-2 gap-4"
                 >
-                    <InputFields title="Nama Tamu" name="nama_tamu" />
-                    <InputFields title="Alamat" name="alamat" />
-                    <InputFields title="No Telepon" name="no_telpon" />
-                    <InputFields title="Keperluan" name="keperluan" />
+                    <InputFields title="Nama Tamu" name="nama_tamu" error={errors.nama_tamu ? errors.nama_tamu[0] : ""}/>
+                    <InputFields title="Alamat" name="alamat" error={errors.alamat ? errors.alamat[0] : ""}/>
+                    <InputFields title="No Telepon" name="no_telpon" type="number" error={errors.no_telepon ? errors.no_telepon[0] : ""}/>
+                    <InputFields title="Keperluan" name="keperluan" error={errors.keperluan ? errors.keperluan[0] : ""}/>
                     <InputFields
                         title="Instansi Asal"
                         name="institusi_id"
@@ -113,6 +143,7 @@ export default function GuestBookDetail() {
                             setSelectedInstitution(value)
                         }
                         addItemPath="/institution/addData"
+                        error={errors.institusi_id ? errors.institusi_id[0] : ""}
                     />
                     <SelectFields
                         title="Divisi Tujuan"
@@ -128,6 +159,7 @@ export default function GuestBookDetail() {
                             selectedInstitution,
                             "alamat",
                         )}
+                        error={errors.alamat_institusi ? errors.alamat_institusi[0] : ""}
                     />
                     <InputFields
                         title="Kontak Instansi"
@@ -136,6 +168,7 @@ export default function GuestBookDetail() {
                             selectedInstitution,
                             "kontak",
                         )}
+                        error={errors.kontak_institusi ? errors.kontak_institusi[0] : ""}
                     />
                     <button
                         className="flex w-full items-center justify-center gap-x-2 rounded bg-primary p-3 font-medium text-gray hover:bg-opacity-90 disabled:cursor-not-allowed disabled:bg-opacity-75 col-span-2"
