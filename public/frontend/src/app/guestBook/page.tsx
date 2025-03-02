67bc9f87f996863f606d4dfc1e8f1a6515c3e579
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-03-03 06:04:24 +0700

    UPDATE: Intgrated guest book with guest book query handler

--- public/frontend/src/app/guestBook/page.tsx
+++ public/frontend/src/app/guestBook/page.tsx
@@ -4,51 +4,64 @@ import Breadcrumb from "@/components/Breadcrumb";
 import InputFields from "@/components/Fields/InputFields";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import React, { FormEvent, useEffect, useState } from "react";
-import { useDispatch, useStore } from "react-redux";
+import { useStore } from "react-redux";
 import { getDateTime, trimText } from "@/utils/data";
 import SelectFields from "@/components/Fields/SelectFields";
-import { GUEST_BOOK_DEFAULT_DATA } from "@/utils/constans";
 import { toast } from "react-toastify";
 import useFetch from "@/hooks/useFetch";
-import { fetchGuestBook } from "@/services/common";
-import { setGuestBook } from "@/store/servicesSlice";
 import { z } from "zod";
+import { useGetGuestBooksQuery } from "@/services/guestBook";
+import { useGetInstitutionsQuery } from "@/services/institution";
+import { useGetDivisionsQuery } from "@/services/division";
 
 export default function GuestBookDetail() {
-    const dispatch = useDispatch()
     const [isPending, fetchCaller] = useFetch();
     const [errors, setErrors] = useState({});
     const state = useStore().getState();
     const authState = state.auth;
-    const servicesState = state.services;
 
-    const [lastGuest, _] = useState(servicesState.guestBook[0] || null)
+    const { data: guestBooksData, isLoading: guestBookState } =
+        useGetGuestBooksQuery();
+    const { data: institutionsData, isLoading: institutionState } =
+        useGetInstitutionsQuery();
+    const { data: divisionsData, isLoading: divisionState } =
+        useGetDivisionsQuery();
+
+    const [lastGuest, _] = useState("" || null);
 
     const [selectedInstitution, setSelectedInstitution] = useState("");
 
     const schema = z.object({
-            nama_tamu: z.string().min(1, "Nama tidak boleh kosong!"),
-            alamat: z.string().min(1, "Alamat tidak boleh kosong!"),
-            no_telepon: z.number({ invalid_type_error: "Nomor telepon tidak valid"}).min(1, "No Telepon tidak boleh kosong!"),
-            keperluan: z.string().min(1, "Keperluan tidak boleh kosong!"),
-            institusi_id: z.string().min(1, "Institusi tidak boleh kosong!"),
-            alamat_institusi: z.string().min(1, "Alamat Institusi tidak boleh kosong!"),
-            kontak_institusi: z.string().min(1, "Kontak Institusi tidak boleh kosong!"),
-        });
-    
-
-    const syncGuestBookData = async() => {
-        dispatch(setGuestBook(await fetchGuestBook()))
-    }
+        nama_tamu: z.string().min(1, "Nama tidak boleh kosong!"),
+        alamat: z.string().min(1, "Alamat tidak boleh kosong!"),
+        no_telepon: z.preprocess(
+            (val) => {
+              if (typeof val === "number") return val.toString(); // Convert number to string
+              if (typeof val === "string") return val.trim(); // Trim spaces
+              return val; // Pass other values unchanged
+            },
+            z.string()
+              .min(1, "No Telepon tidak boleh kosong!")
+              .refine((val) => /^(\+62|0)\d{9,13}$/.test(val), {
+                message: "Nomor telepon tidak valid",
+              })
+          ),
+        keperluan: z.string().min(1, "Keperluan tidak boleh kosong!"),
+        institusi_id: z.string().min(1, "Institusi tidak boleh kosong!"),
+        alamat_institusi: z
+            .string()
+            .min(1, "Alamat Institusi tidak boleh kosong!"),
+        kontak_institusi: z
+            .string()
+            .min(1, "Kontak Institusi tidak boleh kosong!"),
+    });
 
     const getInstitutionData = (name: string, type: string) => {
         if (!name) return;
 
-        const institutions = servicesState.institutions;
+        if (institutionsData.data.length == 0) return "";
 
-        if (institutions.length == 0) return "";
-
-        const res = institutions.filter(
+        const res = institutionsData.data.filter(
             (institution) => institution.nama == name,
         );
 
@@ -59,65 +72,59 @@ export default function GuestBookDetail() {
 
     const handlePostData = async (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-    
+
         const form = event.currentTarget;
         const data = new FormData(form);
         const currentInstitution = data.get("institusi_id");
-
-        const res = schema
-            .safeParse({ 
-                nama_tamu: data.get("nama_tamu"),
-                alamat: data.get("alamat"),
-                no_telepon: data.get("no_telepon"),
-                keperluan: data.get("keperluan"),
-                institusi_id: data.get("institusi_id"),
-                alamat_institusi: data.get("alamat_institusi"),
-                kontak_institusi: data.get("kontak_institusi"),
-            })
-            .error?.flatten().fieldErrors;
-
-        if (Object.keys(res).length >= 1) {
-            setErrors(res);
+        
+        const validation_res = schema
+        .safeParse({
+            nama_tamu: data.get("nama_tamu"),
+            alamat: data.get("alamat"),
+            no_telepon: data.get("no_telepon"),
+            keperluan: data.get("keperluan"),
+            institusi_id: data.get("institusi_id"),
+            alamat_institusi: data.get("alamat_institusi"),
+            kontak_institusi: data.get("kontak_institusi"),
+        })
+        .error?.flatten().fieldErrors;
+        
+        if (Object.keys(validation_res).length >= 1) {
+            toast.error("Data tidak valid!", { position: "top-right" })
+            setErrors(validation_res);
             return;
         }
-    
-        servicesState.institutions.forEach((institution) => {
+
+        institutionsData.data.forEach((institution) => {
             if (institution.nama === currentInstitution) {
                 data.delete("institusi_id");
                 data.append("institusi_id", institution.id);
             }
         });
-    
-        await fetchCaller(`buku-tamu`, {
+
+        const res = await fetchCaller(`buku-tamu`, {
             method: "POST",
             headers: {
                 Authorization: `Bearer ${authState.token}`,
             },
             body: data,
-        })
-            .then((res) => {
-                toast.success("Terima Kasih, telah mengisi!", {
-                    position: "top-right",
-                });
-    
-                syncGuestBookData();
-    
-                setTimeout(() => {
-                    setSelectedInstitution(""); 
-                    window.location.reload();
-                }, 1000);
-            })
-            .catch(() => {
-                toast.error("Galat saat menambahkan data!", {
-                    position: "top-right",
-                });
-            });
-    };
-    
+        });
 
-    useEffect(() => {
-        syncGuestBookData()
-    }, [])
+
+        if (!res.ok) {
+            console.log(res);
+            toast.error("Galat saat menambahkan data", {
+                position: "top-right",
+            });
+            return;
+        }
+
+        toast.success("Terimakasih telah mengisi!", { position: "top-right" });
+
+        setTimeout(() => {
+            window.location.reload();
+        }, 1000);
+    };
 
 
     return (
@@ -129,28 +136,55 @@ export default function GuestBookDetail() {
                     onSubmit={handlePostData}
                     className="grid grid-cols-2 gap-4"
                 >
-                    <InputFields title="Nama Tamu" name="nama_tamu" error={errors.nama_tamu ? errors.nama_tamu[0] : ""}/>
-                    <InputFields title="Alamat" name="alamat" error={errors.alamat ? errors.alamat[0] : ""}/>
-                    <InputFields title="No Telepon" name="no_telpon" type="number" error={errors.no_telepon ? errors.no_telepon[0] : ""}/>
-                    <InputFields title="Keperluan" name="keperluan" error={errors.keperluan ? errors.keperluan[0] : ""}/>
+                    <InputFields
+                        title="Nama Tamu"
+                        name="nama_tamu"
+                        error={errors.nama_tamu ? errors.nama_tamu[0] : ""}
+                    />
+                    <InputFields
+                        title="Alamat"
+                        name="alamat"
+                        error={errors.alamat ? errors.alamat[0] : ""}
+                    />
+                    <InputFields
+                        title="No Telepon"
+                        name="no_telpon"
+                        type="phone"
+                        error={errors.no_telepon ? errors.no_telepon[0] : ""}
+                    />
+                    <InputFields
+                        title="Keperluan"
+                        name="keperluan"
+                        error={errors.keperluan ? errors.keperluan[0] : ""}
+                    />
                     <InputFields
                         title="Instansi Asal"
                         name="institusi_id"
-                        autoCompleteData={servicesState.institutions.map(
-                            (field) => field.nama,
-                        )}
+                        defaultValue={selectedInstitution}
+                        autoCompleteData={
+                            institutionsData?.data.map((field) => field.nama) ||
+                            []
+                        }
                         onSelectedAutoComplete={(value: string) =>
                             setSelectedInstitution(value)
                         }
-                        addItemPath="/institution/addData"
-                        error={errors.institusi_id ? errors.institusi_id[0] : ""}
+                        error={
+                            errors.institusi_id ? errors.institusi_id[0] : ""
+                        }
                     />
                     <SelectFields
                         title="Divisi Tujuan"
                         name="divisi_id"
-                        options={servicesState.divisions.map((division) => {
-                            return { name: division.nama, value: division.id };
-                        })}
+                        options={
+                            divisionsData
+                                ? divisionsData.data.map((division) => {
+                                      return {
+                                          name: division.nama,
+                                          value: division.id,
+                                      };
+                                  })
+                                : []
+                        }
                     />
                     <InputFields
                         title="Alamat Instansi"
@@ -159,7 +193,11 @@ export default function GuestBookDetail() {
                             selectedInstitution,
                             "alamat",
                         )}
-                        error={errors.alamat_institusi ? errors.alamat_institusi[0] : ""}
+                        error={
+                            errors.alamat_institusi
+                                ? errors.alamat_institusi[0]
+                                : ""
+                        }
                     />
                     <InputFields
                         title="Kontak Instansi"
@@ -168,10 +206,14 @@ export default function GuestBookDetail() {
                             selectedInstitution,
                             "kontak",
                         )}
-                        error={errors.kontak_institusi ? errors.kontak_institusi[0] : ""}
+                        error={
+                            errors.kontak_institusi
+                                ? errors.kontak_institusi[0]
+                                : ""
+                        }
                     />
                     <button
-                        className="flex w-full items-center justify-center gap-x-2 rounded bg-primary p-3 font-medium text-gray hover:bg-opacity-90 disabled:cursor-not-allowed disabled:bg-opacity-75 col-span-2"
+                        className="col-span-2 flex w-full items-center justify-center gap-x-2 rounded bg-primary p-3 font-medium text-gray hover:bg-opacity-90 disabled:cursor-not-allowed disabled:bg-opacity-75"
                         type="submit"
                         disabled={isPending}
                     >
