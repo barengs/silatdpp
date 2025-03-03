1c16907867a527d396ccf25e8f68e768861a3470
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-03-03 12:37:51 +0700

    UPDATE: Added update division handler for division

--- public/frontend/src/services/division.ts
+++ public/frontend/src/services/division.ts
@@ -4,11 +4,20 @@ import { baseApiSlice } from "./base";
 export const divisionApi = baseApiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getDivisions: builder.query({
-      query: () => "/divisi"
+      query: () => "/divisi",
+      providesTags: ['Divisions']
     }),
     getDivisionById: builder.query({
       query: (id) => `/divisi/${id}`
     }),
+    updateDivision: builder.mutation({
+      query: ({ id, ...updatedData}) => ({
+        url: `/divisi/${id}`,
+        method: "PUT",
+        body: updatedData,
+      }),
+      invalidatesTags: ['Divisions']
+    })
   })
 })
 
