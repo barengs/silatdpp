450ef9e789d798bc2edfd82271147f17281bfd66
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-03-03 12:37:31 +0700

    UPDATE: Added header for every fetch that requires Authorization header

--- public/frontend/src/services/base.ts
+++ public/frontend/src/services/base.ts
@@ -1,9 +1,19 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery, RootState } from "@reduxjs/toolkit/query/react";
 
 export const baseApiSlice = createApi({
     reducerPath: "persistedApi",
     baseQuery: fetchBaseQuery({ 
         baseUrl: process.env.NEXT_PUBLIC_BASE_API_URL,
+        prepareHeaders: (headers, {getState, endpoint}) => {
+            const token = (getState() as RootState).auth.token
+            const needsAuth = ['updateDivision']
+
+            if (token && needsAuth.includes(endpoint)) {
+                headers.set('Authorization', `Bearer ${token}`)
+            }
+
+            return headers
+        }
     }),
     endpoints: () => ({})
 })
