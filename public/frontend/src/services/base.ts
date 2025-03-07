4acd18e6bedb73b2265acd9fe6d94fc75ff88266
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-03-08 04:04:24 +0700

    UPDATE: Added news mutation to base store

--- public/frontend/src/services/base.ts
+++ public/frontend/src/services/base.ts
@@ -15,6 +15,6 @@ export const baseApiSlice = createApi({
             return headers
         }
     }),
-    tagTypes: ["Divisions", "Institutions", "Transportations", "Budgets", "Partners", "GuestBooks", "Permissions", "Roles", "Certificates"],
+    tagTypes: ["Divisions", "Institutions", "Transportations", "Budgets", "Partners", "GuestBooks", "Permissions", "Roles", "Certificates", "News"],
     endpoints: () => ({})
 })
