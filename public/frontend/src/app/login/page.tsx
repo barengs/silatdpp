67f8b64653e44629f1a94d1719ad4df6431bb865
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-03-15 19:33:23 +0700

    FIX: Resize the layout

--- public/frontend/src/app/login/page.tsx
+++ public/frontend/src/app/login/page.tsx
@@ -57,54 +57,59 @@ export default function Page() {
     useEffect(() => console.log(newsData), [isLoading]);
 
     return (
-        <div className="flex w-full min-h-screen">
-            <div className="w-full flex flex-col md:flex-row items-center justify-center flex-1">
-                <div className="flex-1 hidden lg:flex justify-center items-center p-4 w-full lg:min-w-[500px] h-full bg-primary">
-                    <div className="bg-white py-8 px-12 rounded-md">
+        <div className="flex min-h-screen w-full">
+            <div className="flex w-full flex-1 flex-col items-center justify-center md:flex-row">
+                <div className="hidden h-full w-full flex-1 items-center justify-center bg-primary p-4 lg:flex lg:min-w-[500px]">
+                    <div className="rounded-md bg-white px-12 py-8">
                         <h1 className="font-semibold text-black-2 lg:text-xl">
                             Berita Terbaru
                         </h1>
 
-                        <img src={newsData ? newsData.data[0].gambar : ""} alt="berita" />
+                        <img
+                            src={newsData ? newsData.data[0].gambar : ""}
+                            alt="berita"
+                        />
 
-                        <div className="h-full flex flex-col mt-12 text-left">
+                        <div className="mt-12 flex h-full flex-col text-left">
                             <p> {newsData ? newsData.data[0].judul : ""}</p>
                             <p>{newsData ? newsData.data[0].isi : ""}</p>
                         </div>
                     </div>
                 </div>
 
-                <div className="flex-1 w-full flex flex-col justify-center px-4">
-                    <h1 className="text-center text-3xl font-semibold text-black">
-                        Login
-                    </h1>
-                    <form onSubmit={onSubmit} className="mt-16 space-y-12">
-                        <InputFields
-                            title="Email"
-                            name="email"
-                            error={errors.messege}
-                        />
-                        <InputFields
-                            title="Password"
-                            name="password"
-                            type="password"
-                            error={errors.messege}
-                        />
-                        <button
-                            className="flex w-full items-center justify-center gap-x-2 rounded bg-primary p-3 font-medium text-gray hover:bg-opacity-90 disabled:cursor-not-allowed disabled:bg-opacity-75"
-                            type="submit"
-                            disabled={isPending}
-                        >
-                            {isPending ? (
-                                <>
-                                    <div className="size-4 animate-spin rounded-full border-2 border-white border-t-transparent"></div>
-                                    Memeriksa identitas
-                                </>
-                            ) : (
-                                <>Masuk</>
-                            )}
-                        </button>
-                    </form>
+                <div className="flex w-full flex-1 flex-col justify-center px-4">
+                    <div className="w-[450px] mx-auto">
+                        <h1 className="text-center text-3xl font-semibold text-black">
+                            Login
+                        </h1>
+                        <form onSubmit={onSubmit} className="mt-16 space-y-12">
+                            <InputFields
+                                title="Email"
+                                name="email"
+                                error={errors.messege}
+                            />
+                            <InputFields
+                                title="Password"
+                                name="password"
+                                type="password"
+                                error={errors.messege}
+                            />
+                            <button
+                                className="flex w-full items-center justify-center gap-x-2 rounded bg-primary p-3 font-medium text-gray hover:bg-opacity-90 disabled:cursor-not-allowed disabled:bg-opacity-75"
+                                type="submit"
+                                disabled={isPending}
+                            >
+                                {isPending ? (
+                                    <>
+                                        <div className="size-4 animate-spin rounded-full border-2 border-white border-t-transparent"></div>
+                                        Memeriksa identitas
+                                    </>
+                                ) : (
+                                    <>Masuk</>
+                                )}
+                            </button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
