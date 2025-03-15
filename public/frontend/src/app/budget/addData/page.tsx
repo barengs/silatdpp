74e6f165ce552d3060648fd521cc95c9f333a7e5
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-03-16 03:59:55 +0700

    UPDATE: Added form validation

--- public/frontend/src/app/budget/addData/page.tsx
+++ public/frontend/src/app/budget/addData/page.tsx
@@ -5,20 +5,36 @@ import InputFields from "@/components/Fields/InputFields";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import useFetch from "@/hooks/useFetch";
 import { useRouter } from "next/navigation";
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
 import { useStore } from "react-redux";
 import { toast } from "react-toastify";
+import { z } from "zod";
 
 const BudgetAddDataPage: React.FC = () => {
-    const [, fetchCaller] = useFetch();
+    const [isPending, fetchCaller] = useFetch();
+    const [errors, setErrors] = useState({})
     const storeState = useStore().getState();
     const router = useRouter();
+
+    const formSchema = z.object({
+        biaya: z.string().min(1, "Harap masukkan nama tingkat biaya")
+    })
 
     const handlePostData = async (event: FormEvent<HTMLFormElement>) => {
 
         event.preventDefault()
 
         const form = new FormData(event.currentTarget);
+
+
+        const validationResult = formSchema.safeParse({
+            biaya: form.get("biaya")
+        })
+
+        if (!validationResult.success) {
+            setErrors(validationResult.error.flatten().fieldErrors)
+            return
+        }
 
         await fetchCaller("biaya", {
             method: "post",
@@ -48,12 +64,20 @@ const BudgetAddDataPage: React.FC = () => {
                 <InputFields
                     title="Tingkat Biaya"
                     name="biaya"
+                    error={errors.biaya ? errors.biaya[0] : ""}
                 />
                 <div className="col-span-2">
                     <button
                         className="w-max flex justify-center rounded bg-primary p-3 font-medium text-gray hover:bg-opacity-90"
                     >
-                        Tambahkan Biaya
+                        {isPending ? (
+                        <div className="flex gap-x-4">
+                            <div className="size-4 animate-spin rounded-full border-2 border-white border-t-transparent"></div>
+                            Menambahkan
+                        </div>
+                    ) : (
+                        <> Tambah Biaya</>
+                    )}
                     </button>
                 </div>
             </form>
