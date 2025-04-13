579e97d24ab0a244e97f7727c596b1d6ba4a172a
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-04-13 10:23:25 +0700

    CLEANING

--- public/frontend/src/app/news/addData/page.tsx
+++ public/frontend/src/app/news/addData/page.tsx
@@ -1,103 +1,138 @@
-"use client"
+"use client";
 
 import Breadcrumb from "@/components/Breadcrumb";
-import FilesFields from "@/components/Fields/FileFields";
-import InputFields from "@/components/Fields/InputFields";
-import TextFields from "@/components/Fields/TextFields";
+import { Editor } from "@tinymce/tinymce-react";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import useFetch from "@/hooks/useFetch";
 import { useUpdateNewsMutation } from "@/services/news";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { useStore } from "react-redux";
 import { toast } from "react-toastify";
 import { z } from "zod";
 
 const Page: React.FC = () => {
-    const store = useStore()
-    const authState = store.getState().auth
+    const store = useStore();
+    const authState = store.getState().auth;
 
-    const [file, setFile] = useState<File[]>([])
-    
-    const [isPending, fetchCaller] = useFetch()
-    const [updateNews] = useUpdateNewsMutation()
-    const [errors, setErrors] = useState({})
-    const router = useRouter()
+    const editorRef = useRef()
 
-     const schema = z.object({
-            judul: z.string().min(1, "Judul tidak boleh kosong!"),
-            isi: z.string().min(1, "Isi tidak boleh kosong!"),
-            gambar: z.instanceof(File),
-        });
+    const [file, setFile] = useState<File[]>([]);
+
+    const schema = z.object({
+        judul: z.string().min(1, "Judul tidak boleh kosong!"),
+        isi: z.string().min(1, "Isi tidak boleh kosong!"),
+        gambar: z.instanceof(File),
+    });
 
     const handlePostData = async (event: FormEvent<HTMLFormElement>) => {
-            event.preventDefault();
-    
-            const formData = event.currentTarget;
-    
-            const data = new FormData(formData);
-    
-            const validation_res = schema
-                .safeParse({ judul: data.get("judul"), isi: data.get("isi"), gambar: file})
-                
-            if (!validation_res.success) {
-                toast.error("Data tidak valid", { position: "top-right"})
-                setErrors(validation_res.error?.flatten().fieldErrors)
-                return
-            }
-    
+        event.preventDefault();
 
-            data.append("gambar", file)
-    
-            const res = await fetchCaller("berita", {
-                method: "POST",
-                headers: {
-                    Authorization: `Bearer ${authState.token}`,
-                },
-                body: data,
-            })
+        const formData = event.currentTarget;
 
-            if (!res.ok) {
-                toast.error("Galat saat menambahkan data", {
-                    position: "top-right",
-                });
+        const data = new FormData(formData);
 
-                console.log(res)
+        const validation_res = schema.safeParse({
+            judul: data.get("judul"),
+            isi: data.get("isi"),
+            gambar: file,
+        });
 
-                return
-            }
+        if (!validation_res.success) {
+            toast.error("Data tidak valid", { position: "top-right" });
+            setErrors(validation_res.error?.flatten().fieldErrors);
+            return;
+        }
 
-            toast.success("Data Berita berhasil ditambahkan!", {
+        data.append("gambar", file);
+
+        const res = await fetchCaller("berita", {
+            method: "POST",
+            headers: {
+                Authorization: `Bearer ${authState.token}`,
+            },
+            body: data,
+        });
+
+        if (!res.ok) {
+            toast.error("Galat saat menambahkan data", {
                 position: "top-right",
             });
-            
-            setTimeout(() => window.location.reload(), 2000)
-        };
+
+            console.log(res);
+
+            return;
+        }
+
+        toast.success("Data Berita berhasil ditambahkan!", {
+            position: "top-right",
+        });
+
+        setTimeout(() => window.location.reload(), 2000);
+    };
 
     return (
         <DefaultLayout>
             <Breadcrumb pageName="Tambah Berita" />
-            <form onSubmit={handlePostData} className="flex flex-col lg:grid lg:grid-cols-2 gap-9 rounded-sm border border-stroke bg-white px-6.5 py-4 shadow-default dark:border-strokedark dark:bg-boxdark">
-                <InputFields title="Judul Berita" name="judul" error={errors.judul ? errors.judul[0] : ""}/>
-                <TextFields title="Isi Berita" name="isi" error={errors.isi ? errors.isi[0] : ""}/>
-                <FilesFields title="Gambar" setter={(file: File[]) => setFile(file[0])} error={errors.gambar ? errors.gambar[0] : ""} />
-                <button
-                    className="flex w-max col-span-2 columns-2 items-center justify-center gap-x-2 rounded bg-primary p-3 font-medium text-gray hover:bg-opacity-90 disabled:cursor-not-allowed disabled:bg-opacity-75"
-                    type="submit"
-                    disabled={isPending}
-                >
-                    {isPending ? (
-                        <>
-                            <div className="size-4 animate-spin rounded-full border-2 border-white border-t-transparent"></div>
-                            Menambahkan Data
-                        </>
-                    ) : (
-                        <>Tambahkan Berita</>
-                    )}
-                </button>
+            <form
+                onSubmit={handlePostData}
+                className="gap-9 rounded-sm border border-stroke bg-white px-6.5 py-4 shadow-default dark:border-strokedark dark:bg-boxdark"
+            >
+                <Editor
+                    ref={editorRef}
+                    apiKey="y68r3jg8eztocsl3akqljuyp3tdqipz0dyskjwz4tzyyoabl"
+                    init={{
+                        plugins: [
+                            // Core editing features
+                            "anchor",
+                            "autolink",
+                            "charmap",
+                            "codesample",
+                            "emoticons",
+                            "image",
+                            "link",
+                            "lists",
+                            "media",
+                            "searchreplace",
+                            "table",
+                            "visualblocks",
+                            "wordcount",
+                            "checklist",
+                            "mediaembed",
+                            "casechange",
+                            "formatpainter",
+                            "pageembed",
+                            "a11ychecker",
+                            "tinymcespellchecker",
+                            "permanentpen",
+                            "powerpaste",
+                            "advtable",
+                            "advcode",
+                            "editimage",
+                            "advtemplate",
+                            "mentions",
+                            "tinycomments",
+                            "tableofcontents",
+                            "footnotes",
+                            "mergetags",
+                            "autocorrect",
+                            "typography",
+                            "inlinecss",
+                        ],
+                        toolbar:
+                            "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat",
+                        tinycomments_mode: "embedded",
+                        tinycomments_author: "Author name",
+                        menubar: false,
+                        mergetags_list: [
+                            { value: "First.Name", title: "First Name" },
+                            { value: "Email", title: "Email" },
+                        ],
+                    }}
+                />
             </form>
         </DefaultLayout>
-    )
-}
+    );
+};
 
 export default Page;
