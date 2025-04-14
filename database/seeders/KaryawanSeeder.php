0f19ba37a42574fa44ccfb80820576fc001c1d28
rofiareiv <kambing.rofi@gmail.com>
2025-04-14 09:48:55 +0700

    update karyawan seeder

--- database/seeders/KaryawanSeeder.php
+++ database/seeders/KaryawanSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use App\Models\Karyawan;
+use Carbon\Carbon;
 use App\Models\User;
+use App\Models\Karyawan;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
