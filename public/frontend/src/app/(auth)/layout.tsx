e3a9dac64022bc29e5f30b1c9b9856170e2d100d
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-04-30 09:22:03 +0700

    UPDATE: Added school headmaster and admin role to access controll

--- public/frontend/src/app/(auth)/layout.tsx
+++ public/frontend/src/app/(auth)/layout.tsx
@@ -1,59 +1,103 @@
-"use client"
+"use client";
 
 import { useRouter, usePathname } from "next/navigation";
 import React, { useEffect } from "react";
 import { useStore } from "react-redux";
 
+const ADMIN_ALLOWED_URL = [
+    "/sppd/",
+    "/sppd/list/",
+    "/guestBook/",
+    "/guestBook/list/",
+    "/news/",
+    "/news/addData/",
+];
 
+const KABID_ALLOWED_URL = ["/sppd/list/", "/news/", "/news/addData/"];
 
-const ADMIN_ALLOWED_URL = ["/sppd/", "/sppd/list/", "/guestBook/", "/guestBook/list/", "/news/", "/news/addData/"]
+const RESEPSIONIS_ALLOWED_URL = [
+    "/sppd/",
+    "/sppd/list/",
+    "/certificate/list/",
+    "/certificate/",
+    "/studentTransfer/",
+    "/studentTransfer/list",
+    "/recomendation/",
+    "/recomendation/list/",
+    "/news/",
+    "/news/addData/",
+];
 
-const KABID_ALLOWED_URL = ["/sppd/list/", "/news/", "/news/addData/"]
+const HEADMASTER_ALLOWED_URL = [
+    "/certificate/list/",
+    "/certificate/",
+    "/studentTransfer/",
+    "/studentTransfer/list",
+    "/recomendation/",
+    "/recomendation/list/",
+];
 
-const RESEPSIONIS_ALLOWED_URL = ["/sppd/", "/sppd/list/", "/certificate/list/", "/certificate/", "/studentTransfer/", "/studentTransfer/list", "/recomendation/", "/recomendation/list/", "/news/", "/news/addData/"]
-
-
+const SCHOOL_ADMIN_ALLOWED_URL = [
+    "/certificate/list/",
+    "/certificate/",
+    "/studentTransfer/",
+    "/studentTransfer/list",
+    "/recomendation/",
+    "/recomendation/list/",
+];
 
 export default function AuthLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+    const store = useStore();
+    const authState = store.getState().auth;
 
-    const store = useStore()
-    const authState = store.getState().auth
+    const navigate = useRouter();
 
-    const navigate = useRouter()
-
-    const pathname = usePathname()
+    const pathname = usePathname();
 
     useEffect(() => {
-
-        switch(authState.user.role) {
+        switch (authState.user.role) {
             case "superadmin":
-                return
+                return;
 
             case "administrasi":
-                if (!ADMIN_ALLOWED_URL.includes(pathname)){
-                    navigate.push("/login")
+                if (!ADMIN_ALLOWED_URL.includes(pathname)) {
+                    navigate.push("/login");
                 }
+                return;
 
             case "kabid":
-                if (!KABID_ALLOWED_URL.includes(pathname)){
-                    navigate.push("/login")
+                if (!KABID_ALLOWED_URL.includes(pathname)) {
+                    navigate.push("/login");
                 }
+                return;
 
             case "resepsionis":
-                if (!RESEPSIONIS_ALLOWED_URL.includes(pathname)){
-                    navigate.push("/login")
+                if (!RESEPSIONIS_ALLOWED_URL.includes(pathname)) {
+                    navigate.push("/login");
                 }
+                return;
+
+            case "kepala-sekolah":
+                if (!HEADMASTER_ALLOWED_URL.includes(pathname)) {
+                    navigate.push("/login");
+                }
+                return;
+
+            case "admin-sekolah":
+                if (!SCHOOL_ADMIN_ALLOWED_URL.includes(pathname)) {
+                    navigate.push("/login");
+                }
+                return;
 
             default:
                 // unrecognized role
-                navigate.push("/login")
+                navigate.push("/login");
         }
-    }, [])
-
+    }, []);
 
     return <div>{children}</div>;
 }
