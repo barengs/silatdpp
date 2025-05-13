5934070c960d18d5891e13a4400dce354d2f5a10
rofiareiv <kambing.rofi@gmail.com>
2025-05-14 06:27:02 +0700

    delete frontend

--- public/frontend/src/types/components/Forms.d.ts
+++
@@ -1,5 +0,0 @@
-import { FormEvent, PropsWithChildren } from "react";
-
-export default interface FormPropsType extends PropsWithChildren {
-    onSubmit: (event: FormEvent<HTMLFormElement>) => void
-}
